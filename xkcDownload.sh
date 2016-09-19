bfdd0748a101358af94d547f677ec7a591223bd4
David <worleyd@purdue.edu>
2016-09-19 04:38:12 -0400

    final version

--- xkcDownload.sh
+++ xkcDownload.sh
@@ -6,8 +6,8 @@ if [ $START -gt $END ] || [ $RANGE -gt 20 ] ; then
 	echo "ERROR: Please enter correct indices."
 	exit 1
 fi
-mkdir -p tars
-cd tars
+mkdir -p xkcd_comics
+cd xkcd_comics
 COUNT=$START
 while [ $COUNT -le $END ] ; do	
 	location=$(curl -sL http://xkcd.com/$COUNT | grep -oh 'http://imgs.xkcd.com/comics/[^.]*\.[a-z]*')
@@ -15,7 +15,7 @@ while [ $COUNT -le $END ] ; do
 	let COUNT+=1
 done
 cd ..
-if [ "$ls -A tars/" ] ; then
-	tar -cvf xkcd_comics.tar tars/
+if [ "$ls -A xkcd_comics/" ] ; then
+	tar -cvf xkcd_comics.tar xkcd_comics/
 fi
-	rm -r tars
+	rm -r xkcd_comics/
